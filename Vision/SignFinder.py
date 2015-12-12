8b8038b9b64796230d55f9acf099b7f9223d1eca
teddycool <par@sundback.com>
2015-12-12 14:03:58 +0100

    new signFinder

---
+++ Vision/SignFinder.py
@@ -0,0 +1,28 @@
+__author__ = 'teddycool'
+#Purpose: to find traffic-signs in the delivered frame
+
+import cv2
+import numpy as np
+import time
+
+class SignFinder(object):
+
+    def __init__(self):
+        self._signs = {'Stop', 'Left', 'Right'}
+        self._foundSigns = {}
+        return
+
+
+    def initialize(self):
+        return
+
+    def update(self, frame):
+        return
+
+    def draw(self, frame):
+
+        return frame
+
+
+if __name__ == '__main__':
+    print "Testcode for "
