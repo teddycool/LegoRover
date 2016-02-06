a56742654c3f24d7c9b5ec38e437144340e88921
tgtob <tg.tobias@gmail.com>
2016-02-06 17:15:00 +0100

    tgtest

--- Sensors/RangeSensors.py
+++ Sensors/RangeSensors.py
@@ -1,16 +1,20 @@
 
 import time
 import cv2
+#import RPi.GPIO as GPIO
 
 class RangeSensor(object):
     
-    def __init__(self, gpio, triggerpin, echopin):
+    def __init__(self, gpio, triggerpin, echopin,sample_time = 0.2):
         self._gpio = gpio
         self.TRIG = triggerpin
         self.ECHO = echopin
-        self._lastrange = 0
+        self.distance = 0
+        self.ValueUpdated = False
+        self.startTime = 0
+        self.trigCount = 0
+        self._sample_time = sample_time
 
-        
         print "Distance Measurement initialized for pin: " + str(echopin)
         
         self._gpio.setup(self.TRIG,self._gpio.OUT)
@@ -18,61 +22,48 @@ class RangeSensor(object):
         
         self._gpio.output(self.TRIG, False)
         print "Stabilizing range-sensor on pin: " + str(echopin)
-        time.sleep(2)
+        time.sleep(1)
+        self._gpio.add_event_detect(self.ECHO, self._gpio.BOTH, callback=self.EdgeEchoPin)
+        self.trig()
 
-       
-    def update(self):
+    def EdgeEchoPin(self, channel):
+        if self._gpio.input(self.ECHO) == 1:
+            self.startTime = time.time()
+        else:
+            self.distance = (time.time() - self.startTime) * 17000
+            self.ValueUpdated = True
+            time.sleep(self._sample_time)
+            self.trig()
+
+
+    def trig(self):
+        self.trigCount += 1
+        self.ValueUpdated = False
         self._gpio.output(self.TRIG, True)
         time.sleep(0.01) #Triggertime-high
         self._gpio.output(self.TRIG, False)
-        pulse_start = time.time()
-        echo_start = time.time()
-        echo_end = time.time()
-        #TODO: fix a timeout...
-        while self._gpio.input(self.ECHO)==0:
-            echo_start = time.time()
-            if time .time() - pulse_start > 0.1:
-                break
-        
-        while self._gpio.input(self.ECHO)==1:
-            echo_end = time.time()
-            if time .time() - pulse_start > 0.2:
-                break
-          
-        pulse_duration_max = echo_end - pulse_start
-        pulse_duration_min = echo_start - pulse_start
-        pulse_duration = echo_end - echo_start
-        #distance = high level time * sound-velocity (340M/S) / 2
-        #dataspec suggest to use over 60ms measurement cycle, in order to prevent trigger signal to the echo signal. 
-        distance_max = pulse_duration_max * 17000        
-        distance_max = round(distance_max, 2)    
-        distance_min = pulse_duration_min * 17000        
-        distance_min = round(distance_min, 2) 
-        distance = pulse_duration * 17000        
-        distance = round(distance, 2)
-        self._lastrange = distance
-        print "Distance RangeSensor pin " + str(self.ECHO) + ": " + str(distance)
-        return self._lastrange #_min, distance_max
+
+    def update(self):
+        print "Distance RangeSensor pin " + str(self.ECHO) + ": " + str(self.distance) + " TrigCount=" + str(self.trigCount)
+        self.trigCount = 0
+        return self.distance
 
     def draw(self, frame, name, textstartx, textstarty):
         cv2.putText(frame, name + ": " + str(self._lastrange) + " cm", (textstartx,textstarty),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
         return frame
 
-
-
-        
 if __name__ == '__main__':
     print "Testcode for range sensor"
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BCM)
     rs1 = RangeSensor(GPIO, 23, 24)
-    rs2 = RangeSensor(GPIO, 20, 21)
+#    rs2 = RangeSensor(GPIO, 20, 21)
     try:
         while 1:
             print "RS1: " + str(rs1.update())
             time.sleep(1)
             #print "RS2: " + str(rs2.update())
-            time.sleep(0.5)
+            #time.sleep(0.5)
     except Exception as e:
         print str(e)
         GPIO.cleanup()
