df62f5bbc9353575f9b1b07413fac327b464973b
brixterOne <oscar@familjenblomkvist.se>
2016-07-07 21:52:05 +0200

    Uppdaterad för IR lego robot.

--- MainLoop.py
+++ MainLoop.py
@@ -18,7 +18,7 @@ class MainLoop(object):
         GPIO.setmode(GPIO.BCM)
         self._gpio = GPIO
         self._driver = Driver.Driver(self._gpio)
-        self._sensors = Sensors.Sensors(self._gpio)
+        #self._sensors = Sensors.Sensors(self._gpio)
         self._vision = Vision.Vision((640,480))
 
 
@@ -27,13 +27,13 @@ class MainLoop(object):
         print "Starting timers..."
         self.time=time.time()
         self._vision.initialize()
-        self._sensors.initialize()
+       # self._sensors.initialize()
         self._driver.initialize()
         print "Rover started at ", self.time
 
     def update(self):
         #TODO: add update-freq from config
-        self._sensors.update()
+        #self._sensors.update()
         #TODO: add vision update...
         self._vision.update()
        # self._driver.update(self._sensors.sensorvaluesdict)
@@ -42,7 +42,7 @@ class MainLoop(object):
     def draw(self):
         #TODO: add update-freq from config
         frame = self._vision.getCurrentFrame()
-        frame = self._sensors.draw(frame)
+        #frame = self._sensors.draw(frame)
         frame = self._driver.draw(frame)
         frame = self._vision.draw(frame)
 
