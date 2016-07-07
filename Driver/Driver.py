7a075e744adb912e9e80aeb1824c98ca8c9a8d40
brixterOne <oscar@familjenblomkvist.se>
2016-07-07 21:42:22 +0200

    Uppdaterad för IR lego robot.

--- Driver/Driver.py
+++ Driver/Driver.py
@@ -1,31 +1,33 @@
 __author__ = 'teddycool'
 
-#logic for driving...
-#using MotorControlL298 and sensor values
+# logic for driving...
+# using MotorControlL298 and sensor values
 
 import cv2
+from MotorControl import MotorControlLegoIr8884
 
-from MotorControl import MotorControlL298
+
+# from MotorControl import MotorControlL298
+
 
 
 class Driver(object):
-
-
     def __init__(self, GPIO):
         self._gpio = GPIO
-        #self._gpio.setmode(self._gpio.BCM)
-        self._mc = MotorControlL298.MotorControlL298(self._gpio)
+        # self._gpio.setmode(self._gpio.BCM)
+        # self._mc = MotorControlL298.MotorControlL298(self._gpio)
+        self._mc = MotorControlLegoIr8884.MotorControlLegoIr8884(self._gpio)
         self._drivemodes = ["FwdFast", "FwdSlow", "Stop", "RevFast", "RevSlow", "TRight", "TLeft"]
         self._driverStates = {}
-        #driverStateList = [" " ]
+        # driverStateList = [" " ]
         return
 
     def initialize(self):
-        self._mc.setMotion(0,0)
+        self._mc.setMotion(0, 0)
         return
 
     def update(self, sensorvaluesdict):
-        #TODO: Fix driver-states
+        # TODO: Fix driver-states
         print "Driver update"
         # if sensorvaluesdict["UsFrontLeftDistance"]["Current"] > 50 and sensorvaluesdict["UsFrontRightDistance"]["Current"] > 50:
         #     self._mc.forward()
@@ -43,15 +45,16 @@ class Driver(object):
         #     self._mc.reverse()
         #     self._mc.setSpeed(50)
 
-        self._mc.setMotion(0,0)
+        self._mc.setMotion(0, 0)
         return
 
     def draw(self, frame):
-        #cv2.putText(frame,"Speed: " + str(self._mc._currentSpeed) ,(5,40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
-        cv2.putText(frame,"Motor: " + str(self._mc._controlStates),(5,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+        # cv2.putText(frame,"Speed: " + str(self._mc._currentSpeed) ,(5,40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+        cv2.putText(frame, "Motor: " + str(self._mc._controlStates), (5, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (255, 255, 255), 2)
 
         return frame
 
     def __del__(self):
-        self._mc.setMotion(0,0)
+        self._mc.setMotion(0, 0)
         print "Driver object deleted..."
