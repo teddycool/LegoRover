a2a1a53f959ad5fcb3f7f9bf4e6470c6c259f107
brixterOne <oscar@familjenblomkvist.se>
2016-11-03 21:03:16 +0100

    Some improvmets on vision.

--- Vision/Vision.py
+++ Vision/Vision.py
@@ -39,24 +39,19 @@ class Vision(object):
             self._cam.framerate = 10
             self._rawCapture = PiRGBArray(self._cam, size=resolution)
         self._center = (resolution[0]/2, resolution[1]/2)
-        #self._laserfinder = LaserFinder.LaserFinder()
-       # self._signFinder = SignFinder.SignFinder()
+
         print "Starting streamer..."
         print os.system('sudo mkdir /tmp/stream')
         print os.system('sudo LD_LIBRARY_PATH=/home/pi/mjpg-streamer/mjpg-streamer /home/pi/mjpg-streamer/mjpg-streamer/mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer/www" &')
 
-
         #TODO: check that streamer is running
-
 
     def initialize(self):
         print "Vision initialised"
         self._lastframetime = time.time()
+        self.loopTime = 1
         if (self.test):
             self._imagegenerator = self._cam.capture_continuous(self._rawCapture, format="bgr", use_video_port=True)
-       # self._signFinder.initialize()
-        #tmpImg = cv2.imread('white.jpg')
-        #self.white = cv2.imread('white.jpg', 0) # cv2.cvtColor(tmpImg, cv2.COLOR_BGR2HSV)
 
     def update(self):
         print "Vision update"
@@ -83,43 +78,44 @@ class Vision(object):
 
     def draw(self, frame):
         #self._log.info("Draw started")
-        framerate = 1/(time.time()-self._lastframetime)
-        print "Vision framerate: " + str(framerate)
+        latestLoopTime = time.time()-self._lastframetime
+        self.loopTime = (self.loopTime * 3 + latestLoopTime)/4
+        print "Vision looptime: " + str(self.loopTime)
         self._lastframetime= time.time()
 
+
         # Mask out black color and turn the mask to an black and white picture
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        print "frame " + str(frame.size)
+        self.hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         lower_black = np.array([0, 0, 0])
         upper_black = np.array([100, 100, 100])
-        mask = cv2.inRange(hsv, lower_black, upper_black)
-        print "Mask size: " + str(mask.size)
-
-        print "White size: " + str(self.white.size)
-        #tes = cv2.cvtColor(self.white, cv2.COLOR_BGR2HSV)
-        res = cv2.bitwise_and(self.white, self.white, mask=mask)
+        self.mask = cv2.inRange(self.hsv, lower_black, upper_black)
+        self.res = cv2.bitwise_and(self.white, self.white, mask=self.mask)
 
         # Find regions of black and find their center coordinats
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        (contours, _) = cv2.findContours(img.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
-        print len(contours)
+        (contours, _) = cv2.findContours(self.res.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
         centres = []
+        biggestArea = 0
+        biggestAreaIndex = 0
         for i in range(len(contours)):
-            if cv2.contourArea(contours[i]) < 100:
+            area = cv2.contourArea(contours[i])
+            if area < 100:
                 continue
-            moments = cv2.moments(contours[i])
-            if moments['m00'] != 0:
-                centres.append((int(moments['m10'] / moments['m00']), int(moments['m01'] / moments['m00'])))
-                cv2.circle(frame, centres[-1], 3, (255, 255, 0), -1)
+            if area > biggestArea:
+                biggestArea = area
+                biggestAreaIndex = i
 
-        print "Center coordinats"
-        print centres
+        moments = cv2.moments(contours[biggestAreaIndex])
+        if moments['m00'] != 0:
+            centres.append((int(moments['m10'] / moments['m00']), int(moments['m01'] / moments['m00'])))
+
+        xy = centres[0]
+        print "Area size=" + str(biggestArea) + " x=" + str(xy[0] - 320) + " y=" + str(240 - xy[1])
 
         #draw cross for center of image
         #cv2.line(frame,(self._center[0]-20,self._center[1]),(self._center[0]+20, self._center[1]),(255,255,255),2)
         #cv2.line(frame,(self._center[0],self._center[1]-20),(self._center[0],self._center[1]+20),(255,255,255),2)
 
-        cv2.putText(frame,"Streamer: " + roverconfig["Streamer"]["StreamerImage"] + " Current framerate: " + str(round(framerate, 2)), (5,20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+        #cv2.putText(frame,"Streamer: " + roverconfig["Streamer"]["StreamerImage"] + " Current framerate: " + str(round(framerate, 2)), (5,20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
         #Draw to streamer lib to 'publish'
         cv2.imwrite(roverconfig["Streamer"]["StreamerImage"],frame)
         if roverconfig["Vision"]["WriteCvImageToFile"]:
